48e40908a9f67690697dcac09abdd5af177c0068
AlessioConsolo <alessioconsolo3@gmail.com>
2024-07-09 18:48:54 +0200

    [UPD - JS] Added while cicle to generate 5 random numbers and add them to the array

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 let randomNumbers = [];
 const numbersList = document.getElementById("randomNumbers");
+
+while (randomNumbers.length < 5) {
+  let num = Math.floor(Math.random() * 100 + 1);
+  if (!randomNumbers.includes(num)) {
+    randomNumbers.push(num);
+  }
+}
